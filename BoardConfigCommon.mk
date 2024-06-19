2dae7847528e5290f97ca5971380c53720b8fee2
Michael Bestas <mkbestas@lineageos.org>
2024-06-19 12:26:43 +0200

    sm8150-common: Add a device specific FCM
    Change-Id: Iac6cf1561e0aaa0c0fd234e64ffa34222296954f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,7 @@ TARGET_USES_FOD_ZPOS := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH_COMMON)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH_COMMON)/manifest.xml
