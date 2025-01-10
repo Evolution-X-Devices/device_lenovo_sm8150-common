1fd730ae5b37718521e98ce178b90511e7f4612d
Yumi Yukimura <me.cafebabe@gmail.com>
2025-01-10 21:17:16 +0100

    sm8150-common: Switch to QTI Memtrack AIDL HAL

--- common.mk
+++ common.mk
@@ -208,8 +208,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     hwcomposer.qcom \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
@@ -219,12 +217,12 @@ PRODUCT_PACKAGES += \
     libqdutils \
     libtinyxml \
     libvulkan \
-    memtrack.msmnile \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 # Dolby
 PRODUCT_COPY_FILES += \
